a8bee2b373e56132c5b8c5d42e94911689ced875
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2022-02-07 15:08:14 -0600

    Create Notes13_MathMethods.java

---
+++ src/notes/Notes13_MathMethods.java
@@ -0,0 +1,92 @@
+package notes;
+
+/*
+ * Math Methods
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+public class Notes13_MathMethods {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Constants
+		 * 
+		 * Math.PI
+		 * Math.E
+		 */
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.E + "\n");
+		
+		
+		/*
+		 * Exponents
+		 * Math.pow(base, exponent)
+		 */
+		
+		double num1 = Math.pow(8, 5);
+		System.out.println("8^5 = " + num1 + "\n");
+		
+		
+		
+		/*
+		 * Max and Min
+		 * See Notes09_Accumulation
+		 * 
+		 * When looking for a max and min, set them to extremes to start
+		 * 
+		 * Integer.MAX_VALUE
+		 * Integer.MIN_VALUE	
+		 * 
+		 * 
+		 */
+		
+		int max = Integer.MIN_VALUE; //Set max a low as possible
+		int min = Integer.MAX_VALUE; //Set min a high as possible
+		System.out.println(max);
+		System.out.println(min + "\n");
+		//Now loop to find max and mins
+		
+		
+		
+		/*
+		 * Square Root
+		 * 
+		 * Math.sqrt(number)
+		 */
+		
+		double num2 = Math.sqrt(26);
+		System.out.println(num2 + "\n");
+		
+		
+		/*
+		 * Trig Functions
+		 * sin cos tan
+		 * 
+		 * Math.sin(angle)
+		 * Math.cos(angle)
+		 * Math.tan(angle)
+		 * 
+		 * These are IN RADIANS!!!!!!!!
+		 * 
+		 * Math.toRadians(angle in degrees)
+		 * Math.toDegrees(angle in radians)
+		 * 
+		 * 
+		 */
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
